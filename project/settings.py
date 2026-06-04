826d90583cb1dca71a6c375ba73db0969ec0cb12
KMFHabib <KMFHabib@gmail.com>
2026-06-04 21:45:34 +0300

    add 5 apps in settigns and create module data

--- project/settings.py
+++ project/settings.py
@@ -37,6 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'blog.apps.BlogConfig',
+    'contact.apps.ContactConfig',
+    'home.apps.HomeConfig',
+    'accounts.apps.AccountsConfig',
+    'job.apps.JobConfig',
 ]
 
 MIDDLEWARE = [
